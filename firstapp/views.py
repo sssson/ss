5e1e2454876a9973750254f4f4b6e239efd813d8
Minju <kimminju77786@gmail.com>
2021-08-08 03:20:15 +0900

    웹페이지 연결

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
+from django.http import request
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def helloworld(request):
+    return render(request, 'blog/detail.html')
+
+def siteMain(reqeust):
+    return render(request, 'siteMain.html')
--- firstproject/settings.py
+++ firstproject/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
